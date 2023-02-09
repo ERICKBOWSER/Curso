762020f267ef49e697d5ce6dcef03681058d5d7c
ERICKBOWSER <guerig.eri@gmail.com>
2023-02-09 12:40:50 +0100

    fibonacci terminado y decimal a binario por hacer

---
+++ src/main/java/recursividad/DecimalBinario.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package recursividad;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author guerig
+ */
+public class DecimalBinario {
+
+
+
+    public static int decBinario(int n){
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        do{
+            
+            
+            
+        }while(n != 0 || n != 1);
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/recursividad/Fibonacci.java
+++ src/main/java/recursividad/Fibonacci.java
@@ -11,51 +11,55 @@ package recursividad;
 public class Fibonacci {
     
     public static void main(String[] args) {
-        int res = calcularFibonacci(4);
-        
+        int res = calcularFibonacci(5);
         System.out.println(res);
+        
+        int resIt = fiboIterativo(5);
+        System.out.println(resIt);
+        
+        
+        System.out.println(fiboRecursivo(5));
     }   
     
-    
-    private int numA = 0;
-    private int numB = 1;
-
-    public Fibonacci(int numA, int numB) {
-        this.numA = numA;
-        this.numB = numB;
-    }
-
-    public int getNumA() {
-        return numA;
-    }
-
-    public void setNumA(int numA) {
-        this.numA = numA;
-    }
-
-    public int getNumB() {
-        return numB;
-    }
-
-    public void setNumB(int numB) {
-        this.numB = numB;
-    }
-
-    @Override
-    public String toString() {
-        return "Fibonacci{" + "numA=" + numA + ", numB=" + numB + '}';
+    public static int fiboIterativo(int n){
+        int numero = Math.abs(n);
+        
+        int resultado = 0;
+        int n1 = 0;
+        int n2 = 1;
+        
+        for(int i = 0; i < numero; i++){
+            resultado = n1 + n2;
+            n2 = n1;
+            n1 = resultado;
+                
+        }
+        return resultado;
     }
     
+    public static int fiboRecursivo(int n){ //  Suponemos n positivo o cero
+        if(n < 2 ){
+            return n;
+        }else{
+            return fiboRecursivo(n - 1) + fiboRecursivo(n - 2);
+        }
+    }
+    
+    
+    // Mal
     public static int calcularFibonacci(int max){
         int resA = 0, resB = 1;
-        int resultado = 0;
+        int resultado = 0, resAnterior = 0;
         for(int i = 0; i <= max; i++){
-            resA = Math.abs(resA - 1);
+            resAnterior = resultado;
+            resA = resAnterior;
             System.out.println("Resultado A: " + resA);
-            resB = Math.abs(resB - 2);
-            System.out.println("Resultado B: " + resB);
+            
                         
             resultado = resA + resB;
+            resB = resultado;
+            System.out.println("Resultado B: " + resB);
+            
             System.out.println("Resultado final: " + resultado);
             System.out.println("---------------");
             
